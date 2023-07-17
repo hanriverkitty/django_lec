f2ae9b3e84c7d45fa58ab2a492c3b0e3b32acc09
hanriverkitty <danieltls@naver.com>
2023-07-17 14:20:33 +0900

    3week

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,4 @@
 from django.urls import path 
 from . import views 
-urlpatterns = [ path('', views.index), ]
+urlpatterns = [ path('', views.index),
+               path('<int:content_id>/',views.detail,name='detail'),]
